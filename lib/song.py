b9aa4ef929f38f6ae3b2de2bd57348c8df9023ef
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-07 10:42:59 +0300

    implemented add song to count and add to genres

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,33 @@
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    
+    def __init__(self, name, artist, genre):
+        #"""instantiates with a name, artist, and genre."""
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        #implement s
+        self.add_song_to_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        Song.count +=1
+
+    @classmethod
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+
+
+
+Infamous = Song("Infamous", "Mobb Deep", "Rap")
+
+
+
+
+print(Infamous.name)
+print(Infamous.artist)
+print(Infamous.genre)
