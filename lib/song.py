fa828ab11ba9b7146116c3e99b779583def605d5
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-07 11:17:50 +0300

    final commit

--- lib/song.py
+++ lib/song.py
@@ -3,6 +3,8 @@ class Song:
     count = 0
     genres = []
     artists = []
+    genre_count = {}
+    artist_count = {}
 
     def __init__(self, name, artist, genre):
         #"""instantiates with a name, artist, and genre."""
@@ -14,6 +16,9 @@ class Song:
         self.add_song_to_count()
         self.add_to_genres(genre)
         self.add_to_artists(artist)
+        self.add_to_genre_count(genre)
+        self.add_to_artist_count(artist)
+        
 
 #increases song count by 1 on creating a new song
     @classmethod
@@ -32,11 +37,29 @@ class Song:
         if artist not in cls.artists:
             cls.artists.append(artist)
 
+#add to genre count for each genre
+    @classmethod
+    def add_to_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+
+# Adds an artist count for all artists in the list
+    @classmethod
+    def add_to_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
+
+
 Infamous = Song("Infamous", "Mobb Deep", "Rap")
 
 
 
 
 print(Infamous.name)
-print(Infamous.artist)
+print(Song.artists)
 print(Infamous.genre)
