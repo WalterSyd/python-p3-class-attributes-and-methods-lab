182d7e0c56d8c85c7c9e3bc116745515c4775b3a
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-09-07 10:55:33 +0300

    implemented add to artist

--- lib/song.py
+++ lib/song.py
@@ -2,6 +2,7 @@ class Song:
     
     count = 0
     genres = []
+    artists = []
 
     def __init__(self, name, artist, genre):
         #"""instantiates with a name, artist, and genre."""
@@ -9,9 +10,10 @@ class Song:
         self.artist = artist
         self.genre = genre
 
-        #implement s
+        #update attributes accordingly
         self.add_song_to_count()
         self.add_to_genres(genre)
+        self.add_to_artists(artist)
 
 #increases song count by 1 on creating a new song
     @classmethod
@@ -24,9 +26,11 @@ class Song:
         if genre not in cls.genres:
             cls.genres.append(genre)
 
-#
-    # @classmethod
-
+#adds a new artist to the artist list
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
 
 Infamous = Song("Infamous", "Mobb Deep", "Rap")
 
